393313933f16a0dd91b662c20a4646876a95152b
chayanbank <chayan_bang@hotmail.com>
2017-04-21 18:09:24 +0700

    addnew1

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include('db.php');
     <link rel="stylesheet" href="/resources/demos/style.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
     <script>
     $( function() {
         $( "#datepicker" ).datepicker();
