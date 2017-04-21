030902433267646ec4c8df89a19f268c47433294
chayanbank <chayan_bang@hotmail.com>
2017-04-21 17:59:33 +0700

    addnew

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ include('db.php');
 ?>
 <html>
 <head>
-<title>Appointment</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Appointment</title>
 </head>
 <body>
 </body>
